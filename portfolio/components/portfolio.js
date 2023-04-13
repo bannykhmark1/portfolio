2fb37e10c09f3701d17a84a15343d7b8ee30d9b7
bannykhmark1 <bann.mark@yandex.ru>
2023-04-13 17:35:07 +0500

    добавил swiper

--- portfolio/components/portfolio.js
+++ portfolio/components/portfolio.js
@@ -1,5 +1,45 @@
 import portfolio from "../styles/portfolio.module.scss";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import React from "react";
+import portfolios from "../public/images/portfolio.png";
+import construct from "../public/images/construct.png";
+import calculator from "../public/images/calculator.png";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import Image from "next/image";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 export default function Portfolio() {
-  return <></>;
+  return (
+    <>
+      <div className={portfolio.portfolio_container}>
+        <div className={portfolio.title}>
+          <span>Портфолио</span>
+        </div>
+        <div className={portfolio.swiper}>
+          <Swiper
+            cssMode={true}
+            navigation={true}
+            pagination={true}
+            mousewheel={true}
+            keyboard={true}
+            modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div className={portfolio.calculator}>
+                <Image src={calculator} alt="Picture of the author" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src={construct} alt="Picture of the author" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src={portfolios} alt="Picture of the author" />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </>
+  );
 }
